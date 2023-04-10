3aaa0f970ab379aae8caba2d76e309e7435016b3
ZandiBatch <rdy00328@gmail.com>
2023-04-10 19:35:07 +0900

    GUGU

---
+++ src/GUGUDAN.java
@@ -0,0 +1,12 @@
+public class GUGUDAN {
+    public static void main(String[] args){
+        for(int j=1;j<10;j++){
+            int x=j;
+            for(int i=1;i<10;i++){
+                int y=i*x;
+                System.out.printf(x+"X"+i+"="+y+"\n");
+            }
+            System.out.println();;
+        }
+    }
+}
